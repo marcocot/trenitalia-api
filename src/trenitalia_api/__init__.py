c1cb296bdf321b0fb48bb8013db486c87053fa04
Marco Cotrufo <m.cotrufo@trustfull.com>
2026-05-29 11:26:14 +0200

    feat: expose public API

---
+++ src/trenitalia_api/__init__.py
@@ -0,0 +1,32 @@
+"""Async + sync Python client for the public ViaggiaTreno (Trenitalia) API."""
+
+from ._endpoints import DEFAULT_BASE_URL
+from .client import AsyncClient, Client
+from .exceptions import (
+    APIError,
+    ConnectionError,
+    InvalidResponseError,
+    NotFoundError,
+    TrenitaliaError,
+    UpstreamError,
+)
+from .models import ServiceAlert, StopKind, TrainSearchResult, TrainStatus, TrainStop
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "DEFAULT_BASE_URL",
+    "APIError",
+    "AsyncClient",
+    "Client",
+    "ConnectionError",
+    "InvalidResponseError",
+    "NotFoundError",
+    "ServiceAlert",
+    "StopKind",
+    "TrainSearchResult",
+    "TrainStatus",
+    "TrainStop",
+    "TrenitaliaError",
+    "UpstreamError",
+]
